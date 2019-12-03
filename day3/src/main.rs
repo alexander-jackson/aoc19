ca2e4d42a7d45d9f962d48c6ebbd52792ef7b1fb
Alexander Jackson <alexanderjackson@protonmail.com>
2019-12-03 22:09:07 +0000

    Add solution for Part 1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,44 +1,69 @@
 use std::fs;
+use std::collections::HashSet;
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 struct Coordinate {
     x: i32,
     y: i32,
 }
 
-#[derive(Debug)]
-struct Line {
-    start: Coordinate,
-    end: Coordinate,
+fn simulate(curr: Coordinate, op: (char, i32)) -> Vec<Coordinate> {
+    let (dir, dist) = op;
+    let mut coords: Vec<Coordinate> = Vec::new();
+
+    match dir {
+        'U' => {
+            for i in 1..=dist {
+                coords.push(Coordinate { x: curr.x, y: curr.y + i });
+            }
+        },
+        'D' => {
+            for i in 1..=dist {
+                coords.push(Coordinate { x: curr.x, y: curr.y - i });
+            }
+        },
+        'L' => {
+            for i in 1..=dist {
+                coords.push(Coordinate { x: curr.x - i, y: curr.y });
+            }
+        },
+        'R' => {
+            for i in 1..=dist {
+                coords.push(Coordinate { x: curr.x + i, y: curr.y });
+            }
+        },
+        _ => panic!("Unexpected match"),
+    }
+
+    coords
 }
 
-fn get_wire(line: &str) -> Vec<Line> {
-    let mut wire: Vec<Line> = Vec::new();
-    let mut curr: Coordinate = Coordinate { x: 0, y: 0 };
-    let movements: Vec<(char, i32)> = line.split(',')
+fn get_wire(line: &str) -> HashSet<Coordinate> {
+    // Split the line and get (char, usize)
+    let ops: Vec<(char, i32)> = line.split(',')
         .map(|x| {
             let mut chars = x.chars();
             (chars.next().unwrap(), chars.collect::<String>().parse().unwrap())
         })
         .collect();
 
-    for m in &movements {
-        let next: Coordinate = match m.0 {
-            'U' => Coordinate { x: curr.x, y: curr.y + m.1 },
-            'D' => Coordinate { x: curr.x, y: curr.y - m.1 },
-            'L' => Coordinate { x: curr.x - m.1, y: curr.y },
-            'R' => Coordinate { x: curr.x + m.1, y: curr.y },
-            _ => panic!("Unexpected direction char"),
-        };
+    let mut wire: HashSet<Coordinate> = HashSet::new();
+    let mut curr: Coordinate = Coordinate { x: 0, y: 0 };
 
-        wire.push(Line { start: curr, end: next });
-        curr = next;
+    for op in ops {
+        let coords: Vec<Coordinate> = simulate(curr, op);
+
+        for c in &coords {
+            wire.insert(*c);
+        }
+
+        curr = coords[coords.len() - 1];
     }
 
     wire
 }
 
-fn get_wires(filename: &str) -> (Vec<Line>, Vec<Line>) {
+fn get_wires(filename: &str) -> (HashSet<Coordinate>, HashSet<Coordinate>) {
     // Read the file to a Vec<String>
     let file_lines: Vec<String> = fs::read_to_string(filename)
         .expect("Failed to read the problem input")
@@ -51,7 +76,14 @@ fn get_wires(filename: &str) -> (Vec<Line>, Vec<Line>) {
 }
 
 fn main() {
-    let (wire_a, wire_b): (Vec<Line>, Vec<Line>) = get_wires("input.txt");
+    let (a, b) = get_wires("input.txt");
 
-    dbg!((&wire_a, &wire_b));
+    let matches: HashSet<&Coordinate> = a.intersection(&b).collect();
+
+    let minima: &Coordinate = matches.iter().min_by_key(|c| c.x.abs() + c.y.abs()).unwrap();
+
+    dbg!(&matches);
+    dbg!(&minima);
+
+    println!("Answer is: {}", minima.x.abs() + minima.y.abs());
 }
