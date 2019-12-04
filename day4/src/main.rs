553bda22457ebbfed024e812ea4e24e3224c0b16
Alexander Jackson <alexanderjackson@protonmail.com>
2019-12-04 09:27:22 +0000

    Add function to get digits of a number

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,7 @@
+fn get_digits(input: u32) -> Vec<u32> {
+    input.to_string().chars().map(|d| d.to_digit(10).unwrap()).collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    dbg!(get_digits(114567));
 }
